0700a3b7a706a15c1d4366946c89eec93baa1af4
evinmelek <evinmelek06@gmail.com>
2022-06-17 10:38:42 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,133 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <StatusButton />
+    <StatusButton>
+      <template v-slot:buttontext>
+        <p 
+          class="buttonheader" 
+          :data="statusDataList" > 
+          {{statusDataList.button?.caption}}
+        </p>
+      </template>
+      <template v-slot:status1>
+        <p  
+          class="topleft"
+          :data="statusDataList"
+          :style="{'background-color': statusDataList.status1?.colour}">   
+          {{statusDataList.status1?.caption}}
+        </p>
+      </template>
+      <template v-slot:status2>
+        <p  
+          class="topright"
+          :data="statusDataList"
+          :style="{'background-color': statusDataList.status2?.colour}">   
+          {{statusDataList.status2?.caption}}
+        </p>
+      </template>
+      <template v-slot:status3>
+        <p  
+          class="bottomleft"
+          :data="statusDataList"
+          :style="{'background-color': statusDataList.status3?.colour}">   
+          {{statusDataList.status3?.caption}}  
+        </p>
+      </template>
+      <template v-slot:status4>
+        <p  
+          class="bottomright"
+          :data="statusDataList"
+          :style="{'background-color': statusDataList.status4?.colour}">   
+          {{statusDataList.status4?.caption}} 
+        </p>
+      </template>
+    </StatusButton>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StatusButton from './components/StatusButton.vue'
+//import axios from "axios"
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
-  }
+    StatusButton,
+  },
+   data() {
+    return {
+      statusDataList: {},
+    }
+  },
+  methods: {
+    getStatusData() {
+      fetch("https://mocki.io/v1/9884a9b3-8aa2-461b-9339-da1cc08adcfa")
+        .then(response => response.json())
+        .then(data => (this.statusDataList = data))  
+    },
+  },
+  created() {
+    this.getStatusData()
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.notification .buttonheader{ 
+  position: absolute;
+  top: 80px;
+  right: 140px;
+  padding: 10px 10px;
+  border-radius: 20px; 
+  color: black;
+}
+
+.notification .topright {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  padding: 10px 10px;
+  border-radius: 20px;
+  border: 1px solid black;
+  background-color: white;
+  color: black;
+}
+.notification .topleft {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  padding: 10px 10px;
+  border-radius: 20px;
+  border: 1px solid black;
+  background-color: white;
+  color: black;
+}
+.notification .bottomright {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  padding: 10px 10px;
+  border-radius: 20px;
+  border: 1px solid black;
+  background-color: white;
+  color: black;
+}
+.notification .bottomleft {
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  padding: 10px 10px;
+  border-radius: 20px;
+  border: 1px solid black;
+  background-color: white;
+  color: black;
 }
 </style>
---
+++ src/components/StatusButton.vue
@@ -0,0 +1,115 @@
+<template>
+  <div> 
+    <button class="notification" @click="status1(); status2(); getStatusData();" style="background: url(image.png)">
+      <div v-for="statusData in statusDataList" :key="statusData">
+           
+          <slot name="buttontext"></slot>
+          <slot name="status1"></slot>
+          <slot name="status2"></slot>
+          <slot name="status3"></slot>
+          <slot name="status4"></slot>
+      </div> 
+    </button>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: "StatusButton",
+  data() {
+    return {
+      statusDataList: {}
+    };
+  }, 
+  methods: {
+    getStatusData() {
+      axios.get("https://mocki.io/v1/9884a9b3-8aa2-461b-9339-da1cc08adcfa")
+        .then(response => response)
+        .then(data => (this.statusDataList = data)) 
+    },
+    status1(){
+      axios.get("https://mocki.io/v1/9884a9b3-8aa2-461b-9339-da1cc08adcfa")
+        .then(response => response)
+        .then(data => (this.statusDataList = data))
+      console.log("status1")       
+    },
+    status2(){
+      axios.get("https://mocki.io/v1/9884a9b3-8aa2-461b-9339-da1cc08adcfa")
+        .then(response => response)
+        .then(data => (this.statusDataList = data))   
+      console.log("status2")
+    }
+  },
+};
+</script>
+
+<style scoped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.notification {
+  background-color: #555;
+  color: white;
+  text-decoration: none;
+  height: 200px;
+  width: 400px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  padding: 15px 26px;
+  position: relative;
+  display: inline-block;
+  border-radius: 20px;
+  opacity: 0.8;
+}
+
+.notification:hover {
+  opacity: 1;
+}
+.notification .buttonheader{
+  position: absolute;
+  top: 80px;
+  right: 140px;
+  padding: 10px 10px;
+  border-radius: 20px; 
+  color: black;
+}
+
+.notification .topright {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  padding: 10px 10px;
+  border-radius: 20px;
+  border: 1px solid black; 
+  color: black;
+}
+.notification .topleft {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  padding: 10px 10px;
+  border-radius: 20px;
+  border: 1px solid black; 
+  color: black;
+}
+.notification .bottomright {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  padding: 10px 10px;
+  border-radius: 20px;
+  border: 1px solid black; 
+  color: black;
+}
+.notification .bottomleft {
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  padding: 10px 10px;
+  border-radius: 20px;
+  border: 1px solid black; 
+  color: black;
+}
+</style>
